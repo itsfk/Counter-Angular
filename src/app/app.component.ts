2f238be807d63aa5f7b74e9c4eb939d9bbaa7af3
Faiz <faiz.ggmu@gmail.com>
2018-08-11 16:09:03 +0500

    Increase Method Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Counter';
+  newItem = 'test';
+  number = 0;
+  newItemChanged(event: KeyboardEvent): void {
+    const target = <HTMLInputElement> event.target;
+    this.newItem = target.value;
+    console.log(this.newItem);
+  }
+  Increase() {
+    this.number = this.number + 1;
+  }
 }
